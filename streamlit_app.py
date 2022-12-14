7bfb56338e576011b709a75cebc28274b93cd550
georgemill11 <120366211+georgemill11@users.noreply.github.com>
2022-12-14 18:00:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #create the repeatable code block (called up function)
+streamlit.header("The fruit load list contains:")
 def get_fruityvice_data(this_fruit_choice):
+  with my_cnx.cursor() as my_cur:
+       my_cur.execute("select * from fruit_load_list")
+    
+       return my_cur.fetchall()
+      
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
@@ -51,23 +57,26 @@ streamlit.write('The user entered', fruit_choice)
 # take the json from the response and normalize it
 # output it the screen as a table
 
-
-
-
-
-
 streamlit.stop()
 
 # import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List')
+my_data_rows = get_fruit_load_list()
+streamlit.dataframe(my_data_rows)
+
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("insert into fruit_load_list values ('from streamlit)")
+         return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the list'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   back_from_function = insert_row_snowflake(add_my_fruit)
+   streamlit.text(back_from_function)
+    
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit)")
